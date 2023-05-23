d699da46afcc995a37b67025c989f7bfb3578828
masifmirza927 <masifmirza927@gmail.com>
2023-05-23 16:54:23 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import { useState } from "react"
-import MyFirstComp from "./MyFirstComp"
+
 
 function App() {
-  const [age, setAge] = useState(18);
+  const [fruits, setFruits] = useState(["apple", "mango", "cherry"]);
 
-const updateAge = () => {
-    setAge(20)
-}
+
   return (
     <div className='app'>
-     <h1>welcome to my application - {age} </h1>
-     <button onClick={updateAge} >Update Age</button>
-     <MyFirstComp parent="xyz"  />
+     <ul>
+       {
+        fruits.map( (item) => {
+          return <li>{item}</li>
+        })
+       }
+     </ul>
     </div>
   );
 }
