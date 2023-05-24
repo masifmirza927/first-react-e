0902ab67d5317b66fc726c929b73fc203a59cfc4
masifmirza927 <masifmirza927@gmail.com>
2023-05-24 17:18:42 +0500

    delete item from array

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import './App.css';
 import { useState } from "react"
 
-
 function App() {
   const [fruits, setFruits] = useState(["apple", "cherry"]);
   const [inputValue, setInputValue] = useState("");
-
 
   const updateInputVal = (event) => {
     setInputValue(event.target.value);
   }
 
   const updateArray = () => {
-      const newAr = [...fruits, inputValue];
-      setFruits(newAr);
+    const newAr = [...fruits, inputValue];
+    setFruits(newAr);
   }
 
+ const deleteItem = (targetItem) => {
+    
+    const resutl = fruits.filter( (item) => {
+        return item != targetItem
+    });
+    setFruits(resutl);
+ }
 
   return (
     <div className='app'>
       <input onChange={updateInputVal} type='text' />
       <button onClick={updateArray}>ADD</button>
 
-     <ul>
-       {
-        fruits.map( (item, index) => {
-          return <li key={index}> {item} </li>
-        })
-       }
-     </ul>
+      <ul>
+        {
+          fruits.map((item, index) => {
+            return <li key={index}> {item} <button onClick={ () => { deleteItem(item) } } >X</button> </li>
+          })
+        }
+      </ul>
     </div>
   );
 }
